b6f94ce4b87362dbb563f360f6242f0e442050dc
1MID <lt1stsolomid@gmail.com>
2022-11-09 23:36:56 +0800

    style: on-board page RWD (one-size)

--- src/app/home/pages/on-board/on-board.config.ts
+++ src/app/home/pages/on-board/on-board.config.ts
@@ -4,9 +4,9 @@ export const onBoardConfig = {
     content: "前端工程師和介面設計師，攜手合作拿獎金",
     subContent: "羨慕別人的酷酷網頁動畫？滿足不了同事的許願？動畫技能樹太雜無從下手？",
     attendInfo: [
-        { title: '個人組人數', count: 1158 },
-        { title: '團體組人數', count: 1052 },
-        { title: '報名總人數', count: 41 }
+        { title: '報名總人數', count: 1158 },
+        { title: '個人賽人數', count: 1052 },
+        { title: '團體賽人數', count: 41 }
     ],
     bounty: [
         { prize: "個人獎", reward: "$3,000" },
